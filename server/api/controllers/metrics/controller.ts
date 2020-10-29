1e0744f863196b06c50e71187bac8c2acd264d67
youssef <sokkah.youssef97@gmail.com>
2020-10-29 13:40:24 +0100

    Fixed ram usage

--- server/api/controllers/metrics/controller.ts
+++ server/api/controllers/metrics/controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 export class Controller {
   getMetrics(req: Request, res: Response) {
-     res.status(200).json({"cpuUsage":process.memoryUsage()});
+     res.status(200).json({"ramUsage":process.memoryUsage()});
   }
 }
 export default new Controller();
